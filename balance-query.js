98350c1447df259af6cc993496bd34229c2f17fb
sol wu <apiswswsw@gmail.com>
2020-06-11 23:42:58 -0700

    readme

--- balance-query.js
+++ balance-query.js
@@ -1,6 +1,10 @@
+// this code comes from the video "Coding with Cooper - Your first query - JavaScript" 2020/05/12
+// https://www.youtube.com/watch?v=mEYjBIEc_e8
 const HederaClient = require("./hedera-client");
 const { AccountBalanceQuery } = require("@hashgraph/sdk");
 
+// this doesn't work.  timed out. 
+// we should try newer code
 async function getBalance() {
     const balance = await new AccountBalanceQuery()
         .setAccountId("0.0.65404")
